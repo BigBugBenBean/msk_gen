6d026d364c3bdea398fe4a7f87e03eb548beace4
james.wm.guo@pccw.com <james.wm.guo@pccw.com>
2018-05-29 17:22:51 +0800

    add new

---
+++ src/app/scn-gen/scn-gen-007/scn-gen-007.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router'
+
+@Component ({
+    templateUrl: './scn-gen-007.component.html',
+    styleUrls: ['./scn-gen-007.component.scss']
+})
+
+export class Page7Component {
+    constructor(private router: Router) { }
+
+    nextRoute(next: String) {
+        this.router.navigate([next]);
+    }
+    previousRoute() {
+        this.router.navigate(['/scn-gen/gen002']);
+    }
+
+    slipprint() {
+        this.router.navigate(['/scn-gen/slipprint']);
+    }
+    fingerprint() {
+        this.router.navigate(['/scn-gen/fingerprint']);
+    }
+}
