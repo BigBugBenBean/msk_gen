008cf45f3adff3f6a2b40bcd6fea34818ee1a055
Bill.chen <bill.wf.chen@pccw.com>
2018-07-09 11:16:54 +0800

    clean_viewcard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,5 @@ import { Page1Component } from './scn-sck-001/scn-sck-001.component';
 export const ROUTES: Routes = [
   { path: '', redirectTo: 'scn-gen', pathMatch: 'full'},
   { path: 'scn-gen', loadChildren: './scn-gen#ScreenModule'},
-  { path: 'scn-gen/viewcard', loadChildren: './scn-gen/gen-viewcard#ViewcardModule'},
   { path: '**',    component: NoContentComponent },
 ];
