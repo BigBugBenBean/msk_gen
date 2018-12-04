4750130c1f81d77d722ed6cde1d2be5669590b12
BeanBoy <bill.wf.chen@pccw.com>
2018-12-04 14:36:49 +0800

    去掉debugger

--- src/app/kgen-viewcard/steps/step-processing/step-processing.component.ts
+++ src/app/kgen-viewcard/steps/step-processing/step-processing.component.ts
@@ -205,11 +205,10 @@ export class StepProcessingComponent implements OnInit {
         return;
     }
 
-    exit(promtMessage) {
-        this.storeConfigParam();
-        console.log(22222);
-        this.router.navigate(['/scn-gen/over'], { queryParams: {'err': promtMessage, 'step': 1}});
-    }
+    // exit(promtMessage) {
+    //     this.storeConfigParam();
+    //     this.router.navigate(['/scn-gen/over'], { queryParams: {'err': promtMessage, 'step': 1}});
+    // }
 
     storeConfigParam() {
         this.localStorages.set('fp_tmpl1_in_base64', this.fp_tmpl1_in_base64);
